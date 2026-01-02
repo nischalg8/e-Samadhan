7fdb49465d347b495c09f15e9a93d42e7553f0c1
Pratyush Kattel <pratyushkatel@gmail.com>
2026-01-02 15:21:31 +0545

    .gitignore fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <p><strong>src/App.js</strong></p> and save to reload.
         </p>
         <a
           className="App-link"
